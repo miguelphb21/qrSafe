9e823157b070260d91f866a371b405dd0a871387
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2025-11-13 00:26:18 -0300

    Limpando Pastas Não Utilizadas

--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -10,10 +10,7 @@
 </template>
 
 <script setup>
-// import { RouterView } from "vue-router";
-// import menuLateral from "@/components/menuLateral.vue";
-// import logo from "@/components/logo.vue";
-// import navBar from "@/components/navBar.vue";
+
 import ComponentHeader from "@/components/componentHeader.vue";
 import ComponentMain from "@/components/componentMain.vue";
 import { onMounted } from "vue";
