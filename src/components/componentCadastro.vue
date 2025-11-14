bbb661a322b8e868426cacb6d7a37dcd89a3710a
4drian0 <adrianolima1234577@gmail.com>
2025-11-13 23:52:51 -0300

    wp: fazendo paguina de cadastro

---
+++ src/components/componentCadastro.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ComponentHeader from './componentHeader.vue';
+</script>
+<template>
+    <ComponentHeader></ComponentHeader>
+    
+</template>
--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -5,8 +5,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
   </head>
-  <ComponentHeader/>
-  <ComponentMain/>
+  <!-- <ComponentHeader/>
+  <ComponentMain/> -->
+
+
+
+ <ComponentCadastro/>
 </template>
 
 <script setup>
@@ -14,6 +18,7 @@
 // import menuLateral from "@/components/menuLateral.vue";
 // import logo from "@/components/logo.vue";
 // import navBar from "@/components/navBar.vue";
+import ComponentCadastro from "@/components/componentCadastro.vue";
 import ComponentHeader from "@/components/componentHeader.vue";
 import ComponentMain from "@/components/componentMain.vue";
 import { onMounted } from "vue";
