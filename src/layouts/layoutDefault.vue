4946d9a5b9c23d207a8998e27b9412008311a76e
4drian0 <adrianolima1234577@gmail.com>
2025-11-19 02:18:18 -0300

    wp:qr code inf. finalizado

--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
   </head>
   <ComponentHeader/>
-  <LoginMain/>
+  <InformacoesMain/>
   <ComponentFooter/>
 
 
@@ -16,7 +16,8 @@
 
   import ComponentFooter from "@/components/componentFooter.vue";
   import ComponentHeader from "@/components/componentHeader.vue";
-import LoginMain from "@/views/LoginMain.vue";
+  import InformacoesMain from "@/views/InformacoesMain.vue";
+  // import LoginMain from "@/views/LoginMain.vue";
   // import ComponentMain from "@/views/HomeMain.vue";
   // import ComponentRegistro from "@/views/CadastroMain.vue";
   import { onMounted } from "vue";
---
+++ src/views/InformacoesMain.vue
@@ -0,0 +1,39 @@
+<template>
+    <main class="min-h-screen items-center px-4 bg-[#EEEEEE]">
+        <img src="../assets/images/pessoa.svg" alt=""  class="mx-auto mt-7" >
+        <div class="text-center">
+            <h1 class="font-semibold">João silva</h1>
+        </div>
+        <div class="w-full max-w-md mx-auto p-7 mt-6 mb-25 bg-white border border-gray-200 rounded-lg shadow-lg">
+            <h2 class="text-center font-bold mb-6">Identificação pessoal</h2>
+            <div class=" border border-gray-300 rounded-lg">
+                <p class=" font-semibold mx-auto mt-4 ml-2">Telefone Pessoal</p>
+                <p class="ml-2  text-gray-400">(xx) 9xxx-xxxx</p>
+                <p class=" font-semibold mt-4 ml-2">Contato de Emergência</p>
+                <p class="ml-2 mb-4  text-gray-400">Maria Silva (Mãe) <br> (xx) 9xxx-xxxx</p>
+            </div>
+
+            <div>
+                <h3  class="text-center font-bold mb-7 mt-7">Informações médicas</h3>
+
+                <div class=" border border-gray-300 rounded-lg">
+                    <p class=" font-semibold mx-auto mt-4 nb ml-2">Tipo Sanguíneo </p>
+                    <p class="ml-2  text-gray-400">O+</p>
+                    <p class=" font-semibold mt-4 ml-2">Alergia</p>
+                    <p class="ml-2 text-gray-400">Penicilina</p>
+                    <p class=" font-semibold mt-4 ml-2">Condição</p>
+                    <p class="ml-2 mb-4  text-gray-400">Hipertensão</p>
+                </div>
+            </div>
+
+       
+            <img src="../assets/images/qrLogo.png" alt=""  class="mx-auto w-40 mt-7" >  
+        <div class="p-5 flex justify-center">
+            <button
+            class="w-full bg-black text-[15px] font-bold text-white py-2 rounded-[7px]">
+            Imprimir Cartão
+            </button>
+        </div>
+        </div>
+    </main>
+</template>
